0f6987030828ab50a1e96198f7a43b3ffd324659
Tyler Rudman <tyler.rudman@gmail.com>
2023-03-26 11:08:21 -0400

    fixed small screen responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,108 +19,31 @@ const Header = () => {
           style={{ margin: "0 10px" }}
         />
       </Navbar.Brand>
-      <Nav className="justify-content-end">
-        <Nav.Link
-          as={Link}
-          to="/about"
-          style={{
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}>
-          About Me
-        </Nav.Link>
-        <Nav.Link
-          as={Link}
-          to="/project"
-          style={{
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}>
-          Portfolio
-        </Nav.Link>
-        <Nav.Link
-          href={`mailto:tyler.rudman@gmail.com`}
-          style={{
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}>
-          Contact
-        </Nav.Link>
-        <Nav.Link
-          href="https://docs.google.com/document/d/1LIYh5niK_jV8FDy6X3JiFKgg-QdqDhMhZaD3_V2oHPU/edit?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}>
-          Resume
-        </Nav.Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/about" style={{ fontSize: "20px", fontWeight: "bold" }}>
+            About Me
+          </Nav.Link>
+          <Nav.Link as={Link} to="/project" style={{ fontSize: "20px", fontWeight: "bold" }}>
+            Portfolio
+          </Nav.Link>
+          <Nav.Link
+            href={`mailto:tyler.rudman@gmail.com`}
+            style={{ fontSize: "20px", fontWeight: "bold" }}>
+            Contact
+          </Nav.Link>
+          <Nav.Link
+            href="https://docs.google.com/document/d/1LIYh5niK_jV8FDy6X3JiFKgg-QdqDhMhZaD3_V2oHPU/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontSize: "20px", fontWeight: "bold" }}>
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
 
 export default Header;
-
-// import React from "react";
-// import { Navbar, Nav } from "react-bootstrap";
-// import portfolioPicture from "./images/TR.jpeg";
-
-// const Header = () => {
-//   return (
-//     <Navbar
-//       fixed="top"
-//       expand="lg"
-//       style={{ backgroundColor: "transparent", backdropFilter: "blur(4px)" }}>
-//       <Navbar.Brand href="/">
-//         <img
-//           src={portfolioPicture}
-//           width="50"
-//           height="50"
-//           className="d-inline-block align-top rounded-circle"
-//           alt="My Portfolio"
-//           style={{ margin: "0 10px" }}
-//         />
-//       </Navbar.Brand>
-//       <Nav className="justify-content-end">
-//         <Nav.Link
-//           href="/about"
-//           style={{
-//             fontSize: "20px",
-//             fontWeight: "bold",
-//           }}>
-//           About Me
-//         </Nav.Link>
-//         <Nav.Link
-//           href="/project"
-//           style={{
-//             fontSize: "20px",
-//             fontWeight: "bold",
-//           }}>
-//           Portfolio
-//         </Nav.Link>
-//         <Nav.Link
-//           href={`mailto:tyler.rudman@gmail.com`}
-//           style={{
-//             fontSize: "20px",
-//             fontWeight: "bold",
-//           }}>
-//           Contact
-//         </Nav.Link>
-//         <Nav.Link
-//           href="https://docs.google.com/document/d/1LIYh5niK_jV8FDy6X3JiFKgg-QdqDhMhZaD3_V2oHPU/edit?usp=sharing"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           style={{
-//             fontSize: "20px",
-//             fontWeight: "bold",
-//           }}>
-//           Resume
-//         </Nav.Link>
-//       </Nav>
-//     </Navbar>
-//   );
-// };
-
-// export default Header;
