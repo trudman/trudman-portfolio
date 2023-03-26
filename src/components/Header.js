51c24fb5f1d2bb69b03de15253e6756a425ce5ad
Tyler Rudman <tyler.rudman@gmail.com>
2023-03-26 10:39:06 -0400

    debugging router gh pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import portfolioPicture from "./images/TR.jpeg";
 
 const Header = () => {
@@ -8,7 +9,7 @@ const Header = () => {
       fixed="top"
       expand="lg"
       style={{ backgroundColor: "transparent", backdropFilter: "blur(4px)" }}>
-      <Navbar.Brand href="/">
+      <Navbar.Brand as={Link} to="/">
         <img
           src={portfolioPicture}
           width="50"
@@ -20,7 +21,8 @@ const Header = () => {
       </Navbar.Brand>
       <Nav className="justify-content-end">
         <Nav.Link
-          href="/about"
+          as={Link}
+          to="/about"
           style={{
             fontSize: "20px",
             fontWeight: "bold",
@@ -28,7 +30,8 @@ const Header = () => {
           About Me
         </Nav.Link>
         <Nav.Link
-          href="/project"
+          as={Link}
+          to="/project"
           style={{
             fontSize: "20px",
             fontWeight: "bold",
@@ -59,3 +62,65 @@ const Header = () => {
 };
 
 export default Header;
+
+// import React from "react";
+// import { Navbar, Nav } from "react-bootstrap";
+// import portfolioPicture from "./images/TR.jpeg";
+
+// const Header = () => {
+//   return (
+//     <Navbar
+//       fixed="top"
+//       expand="lg"
+//       style={{ backgroundColor: "transparent", backdropFilter: "blur(4px)" }}>
+//       <Navbar.Brand href="/">
+//         <img
+//           src={portfolioPicture}
+//           width="50"
+//           height="50"
+//           className="d-inline-block align-top rounded-circle"
+//           alt="My Portfolio"
+//           style={{ margin: "0 10px" }}
+//         />
+//       </Navbar.Brand>
+//       <Nav className="justify-content-end">
+//         <Nav.Link
+//           href="/about"
+//           style={{
+//             fontSize: "20px",
+//             fontWeight: "bold",
+//           }}>
+//           About Me
+//         </Nav.Link>
+//         <Nav.Link
+//           href="/project"
+//           style={{
+//             fontSize: "20px",
+//             fontWeight: "bold",
+//           }}>
+//           Portfolio
+//         </Nav.Link>
+//         <Nav.Link
+//           href={`mailto:tyler.rudman@gmail.com`}
+//           style={{
+//             fontSize: "20px",
+//             fontWeight: "bold",
+//           }}>
+//           Contact
+//         </Nav.Link>
+//         <Nav.Link
+//           href="https://docs.google.com/document/d/1LIYh5niK_jV8FDy6X3JiFKgg-QdqDhMhZaD3_V2oHPU/edit?usp=sharing"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           style={{
+//             fontSize: "20px",
+//             fontWeight: "bold",
+//           }}>
+//           Resume
+//         </Nav.Link>
+//       </Nav>
+//     </Navbar>
+//   );
+// };
+
+// export default Header;
