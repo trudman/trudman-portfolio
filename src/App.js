3a260a2a66ee1dc1d4511202d0380e291cc73efe
Tyler Rudman <tyler.rudman@gmail.com>
2023-03-26 10:52:57 -0400

    debugging routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Project from "./components/Project";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Banner />
-        <Routes>
-          <Route path="/about" element={<AboutMe />}></Route>
-          <Route path="/project" element={<Project />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Banner />
+      <Routes>
+        <Route path="/about" element={<AboutMe />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 };
